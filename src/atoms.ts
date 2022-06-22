38e07c8b864791a21547840ae869345b151de269
sukvvon <sukvvon@gmail.com>
2022-06-22 16:06:40 +0900

    Add all files

---
+++ src/atoms.ts
@@ -0,0 +1,23 @@
+import { atom } from "recoil";
+import { recoilPersist } from "recoil-persist";
+
+const { persistAtom } = recoilPersist();
+
+export interface IToDo {
+  id: number;
+  text: string;
+}
+
+interface IToDoState {
+  [key: string]: IToDo[];
+}
+
+export const toDoState = atom<IToDoState>({
+  key: "toDo",
+  default: {
+    "To Do": [],
+    Doing: [],
+    Done: [],
+  },
+  effects_UNSTABLE: [persistAtom],
+});
